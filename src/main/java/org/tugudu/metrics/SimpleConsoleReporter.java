3500ea843433ad6bb8202f19ef54522312335cb2
Thirupathi Reddy Guduru <thirupathi.guduru@cerner.com>
2017-12-21 10:36:41 -0600

    simple metrics example

--- src/main/java/org/tugudu/metrics/SimpleConsoleReporter.java
+++ src/main/java/org/tugudu/metrics/SimpleConsoleReporter.java
@@ -1,11 +1,11 @@
 package org.tugudu.metrics;
 
-import com.codahale.metrics.ConsoleReporter;
-import com.codahale.metrics.Meter;
-import com.codahale.metrics.MetricRegistry;
-
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 
 /**
  * A simple metrics reporter reports on console
@@ -18,16 +18,24 @@ public class SimpleConsoleReporter {
     public static void main(final String[] args) throws InterruptedException {
         final ConsoleReporter consoleReporter = ConsoleReporter.forRegistry(metricRegistry)
                 .convertDurationsTo(TimeUnit.SECONDS).build();
-        consoleReporter.start(1, TimeUnit.SECONDS); // reports onto console for every second
-        final Meter meter = metricRegistry.meter("main");
-        meter.mark();
-        doSomeWork();
+        consoleReporter.start(30, TimeUnit.SECONDS); // reports onto console for every second
+        // final Meter meter = metricRegistry.meter("main");
+        // meter.mark();
+        for (int i = 0; i < 10; i++) {
+            doSomeWork();
+        }
     }
 
     private static void doSomeWork() throws InterruptedException {
-        final Random random = new Random();
-        final int randomValue = random.nextInt(10);
-        System.out.println(randomValue);
-        TimeUnit.SECONDS.sleep(randomValue);
+        final Timer timer = metricRegistry.timer("doSomeWork");
+        final Timer.Context context = timer.time();
+        try {
+            final Random random = new Random();
+            final int randomValue = random.nextInt(10);
+            System.out.println(randomValue);
+            TimeUnit.SECONDS.sleep(randomValue);
+        } finally {
+            context.stop();
+        }
     }
 }
